762026f02d074f2255ffd173316adf8c5dab53d4
somn8687 <46414074+somn8687@users.noreply.github.com>
2023-02-05 10:37:57 +0530

    update on entitites

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { CatagoryModule } from './catagory/catagory.module';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from '../configs/database.module';
+import { GemModule } from './gems/gems';
+import { article } from './articles/entities/articles.entity';
+import { ArticlesModule } from './articles/articles';
+import { CoursesModule } from './courses/courses';
+import { VideosModule } from './videos/videos';
 
 @Module({
   imports: [DatabaseModule, CatagoryModule, GemModule, ArticlesModule,CoursesModule,VideosModule],
